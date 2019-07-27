9e54c24aec95d4a67df01f504e43b14d11d0d6ba
praveen-me <praveenkrsaini0@gmail.com>
2019-07-27 19:44:20 +0530

    Make custom command create

--- bin/cli.js
+++ bin/cli.js
@@ -11,9 +11,17 @@ const currentDirectoryPath = process.cwd();
 const templatePath = "my-app";
 
 const filePath = path.join(currentDirectoryPath, templatePath);
-// console.log(process.cwd());
+program
+  .version(version, "-v, --version")
+  .command("create <type>") // sub-command name, appName = type, required
+  .description("Make a project") // command description
+  // function to execute when command is uses
+  .action(function(appName) {
+    console.log(appName, "Project Name");
+  });
 
-program.version(version, "-v, --version").parse(process.argv);
+// allow commander to parse `process.argv`
+program.parse(process.argv);
 
 // console.log(program);
 // console.log(path.join(__dirname, '/node-react-app'));

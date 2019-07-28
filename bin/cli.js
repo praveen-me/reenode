3d607742791efa0e7a0078bc0e300174ad08250f
praveen-me <praveenkrsaini0@gmail.com>
2019-07-28 15:07:38 +0530

    Add ejs parser

--- bin/cli.js
+++ bin/cli.js
@@ -4,29 +4,40 @@ const path = require("path");
 const fs = require("fs");
 const program = require("commander");
 const inquirer = require("inquirer");
+const ejs = require("ejs");
 const { version } = require("./../package.json");
 const questions = require("./questions");
 
-// TODO:
-// Steps to follow
-// 1 - Create templates folder and inside that create a simple template
-// 2 - Take all the files of the template and try to copy them in to specified directory at a specified location
-
-// Now, I have my first take the template folder location
-
-// Now, I need to copy all the files from templates folder to the specified folder in a particular library
+let reflections = {};
 
 // Current working directory
 const templatePath = path.join(__dirname, `../templates`);
 const reactTemplatePath = templatePath + "/react_+_node";
 
-// Check if the file Object is file or a directory
+/**
+ * Check if the particular file is actual file or a folder
+ *
+ * @param {Object} file File Object
+ *
+ * @return {boolean} TRUE: If file else FALSE
+ */
 const isFile = file => {
   return file.isFile();
 };
 
-// Need to make recursive function that take a source and copy everything recursively
+const parseEjs = (filePath, data) => {
+	const fileContent = fs.readFileSync(filePath).toString();
+	return ejs.render(fileContent, reflections);
+}
 
+/**
+ * Copy files recursively from the source to destination
+ *
+ * @param {string} from Source from where copy starts
+ * @param {string} to Destination where to copy
+ *
+ * @return {void}
+ */
 const copyRecursively = (from, to) => {
   // Make the destination directory
   fs.mkdir(to, err => {
@@ -35,44 +46,54 @@ const copyRecursively = (from, to) => {
 
     // Loop through the file and checking if type is file then just copy else repeat copyRecursively
     files.forEach(file => {
+      const newFrom = `${from}/${file.name}`;
+      const newTo = `${to}/${file.name}`;
+
       // Check file is file or directory
       if (isFile(file)) {
-        // Copy file in a sync way
-        fs.copyFileSync(`${from}/${file.name}`, `${to}/${file.name}`);
+				// Copy file in a sync way
+				if(path.extname(file.name) === '.ejs') {
+					const content = parseEjs(newFrom);
+				} else {
+					fs.copyFileSync(newFrom, newTo);
+				}
+				
       } else {
-        // Else create directory and repeat the procedure
-        // fs.mkdir(`${to}/${file.name}`, err => {
-        //   const newDirLink = `${from}/${file.name}`;
-        //   const newTargetDirectoryLink = `${to}/${file.name}`;
-        //   const filesInsideFolder = fs.readdirSync(newDirLink, {
-        //     withFileTypes: true
-        //   });
-        //   filesInsideFolder.forEach(file =>
-        //     fs.copyFileSync(
-        //       `${newDirLink}/${file.name}`,
-        //       `${newTargetDirectoryLink}/${file.name}`
-        //     )
-        //   );
-        // });
-
-        copyRecursively(`${from}/${file.name}`, `${to}/${file.name}`);
+        // Repeat the procedure again
+        copyRecursively(newFrom, newTo);
       }
     });
   });
 };
 
+/**
+ * Creates the project
+ *
+ * @param {string} appName Name of the project
+ *
+ * @return {void}
+ */
 const createTemplate = appName => {
   const currentDirectoryPath = process.cwd() + `/${appName}`;
   // 1 Make directory on the current path
   copyRecursively(reactTemplatePath, currentDirectoryPath);
 };
 
+/**
+ * Initialize function for boilerplate creation
+ *
+ * @param {string} appName Name of the project
+ *
+ * @return {void}
+ */
 const init = appName => {
   console.log("Welcome to renode.🚀");
 
   inquirer.prompt(questions).then(answers => {
-    console.log("\nOrder receipt:");
-    console.log(JSON.stringify(answers, null, "  "));
+		reflections = {
+			...answers,
+			appName
+		};
     createTemplate(appName);
   });
 };

250aad9c655c4ba01a0cf25733a58f6bca089cf0
praveen-me <praveenkrsaini0@gmail.com>
2019-07-30 01:32:22 +0530

    Intilize the whole template with react and node

---
+++ templates/react_+_node/server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path')
+const ejs = require('ejs');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, './../dist')));
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, './views'));
+
+app.get('/', (req, res) => {
+	res.render('index');
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on ${PORT}`);
+})

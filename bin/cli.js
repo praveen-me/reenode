3e4ed29a557faebbc6cca21a0a555caee0237610
praveen-me <praveenkrsaini0@gmail.com>
2019-08-04 23:32:54 +0530

    Add auto node_modules installation and console info

--- bin/cli.js
+++ bin/cli.js
@@ -8,7 +8,9 @@ const ejs = require("ejs");
 const { version } = require("./../package.json");
 const questions = require("./questions");
 const templates = require("./template-names");
-
+const chalk = require("chalk");
+const exec = require("child_process").exec;
+let error = false;
 let reflections = {};
 
 // Current working directory
@@ -41,62 +43,93 @@ const parseEjs = (filePath, data) => {
  */
 const copyRecursively = (from, to) => {
   // Make the destination directory
-  fs.mkdir(to, err => {
-    // Read file form the source folder
-    const files = fs.readdirSync(from, { withFileTypes: true });
+  try {
+    fs.mkdirSync(to);
+  } catch (e) {
+    if (e.code === "EEXIST") {
+      error = true;
+      console.log(chalk.red.bold("\nDirectory already present. ❌ \n"));
+    }
+    return;
+  }
 
-    // Loop through the file and checking if type is file then just copy else repeat copyRecursively
-    files.forEach(file => {
-      const newFrom = `${from}/${file.name}`;
-      const newTo = `${to}/${file.name}`;
+  const files = fs.readdirSync(from, { withFileTypes: true });
 
-      // Check file is file or directory
-      if (isFile(file)) {
-        
-        // Copy file in a sync way
-        // Check if the file is ejs then need parse template out of it
-        if (path.extname(file.name) === ".ejs") {
-          const mainFileName = file.name.slice(0, file.name.length - 4);
+  // Loop through the file and checking if type is file then just copy else repeat copyRecursively
+  files.forEach(file => {
+    const newFrom = `${from}/${file.name}`;
+    const newTo = `${to}/${file.name}`;
 
-          if (file.name === "index.ejs.ejs" ) {
-            fs.copyFileSync(newFrom, `${to}/${mainFileName}`);
-            return;
-          } 
-          
-          const content = parseEjs(newFrom);
-          fs.writeFileSync(`${to}/${mainFileName}`, content);
+    // Check file is file or directory
+    if (isFile(file)) {
+      // Copy file in a sync way
+      // Check if the file is ejs then need parse template out of it
+      if (path.extname(file.name) === ".ejs") {
+        const mainFileName = file.name.slice(0, file.name.length - 4);
 
-          console.log(`Created ${(to + "/" + mainFileName).replace(new RegExp(process.cwd(), 'g'), '')} 👻`);
-        } else {
-          if (file.name === "package-lock.json" ) return;
-
-          fs.copyFileSync(newFrom, newTo);
-          console.log(`Created ${newTo.replace(new RegExp(process.cwd(), 'g'), '')} 💩`);
-        }
-      } else {
-        const { boilerplate } = reflections;
-        // If some how node_modules and dist directory are left there
-        if (file.name === "node_modules" || file.name === "dist") return;
-
-        if (
-          ((templates.indexOf(boilerplate) === 0 ||
-            templates.indexOf(boilerplate) === 1) &&
-            file.name === "store") ||
-          (templates.indexOf(boilerplate) === 0 &&
-            file.name === "components") ||
-          (templates.indexOf(boilerplate) === 2 &&
-            file.name === "controllers") ||
-          (templates.indexOf(boilerplate) === 2 && file.name === "db") ||
-          (templates.indexOf(boilerplate) === 2 && file.name === "models") ||
-          (templates.indexOf(boilerplate) === 2 && file.name === "routes")
-        )
+        if (file.name === "index.ejs.ejs") {
+          fs.copyFileSync(newFrom, `${to}/${mainFileName}`);
           return;
+        }
 
-        // Repeat the procedure again
-        copyRecursively(newFrom, newTo);
+        const content = parseEjs(newFrom);
+        fs.writeFileSync(`${to}/${mainFileName}`, content);
+
+        console.log(
+          `Created ${(to + "/" + mainFileName).replace(
+            new RegExp(process.cwd(), "g"),
+            ""
+          )} 👻`
+        );
+      } else {
+        if (file.name === "package-lock.json") return;
+
+        fs.copyFileSync(newFrom, newTo);
+        console.log(
+          `Created ${newTo.replace(new RegExp(process.cwd(), "g"), "")} 💩`
+        );
       }
-    });
+    } else {
+      const { boilerplate } = reflections;
+      // If some how node_modules and dist directory are left there
+      if (file.name === "node_modules" || file.name === "dist") return;
+
+      if (
+        ((templates.indexOf(boilerplate) === 0 ||
+          templates.indexOf(boilerplate) === 1) &&
+          file.name === "store") ||
+        (templates.indexOf(boilerplate) === 0 && file.name === "components") ||
+        (templates.indexOf(boilerplate) === 2 && file.name === "controllers") ||
+        (templates.indexOf(boilerplate) === 2 && file.name === "db") ||
+        (templates.indexOf(boilerplate) === 2 && file.name === "models") ||
+        (templates.indexOf(boilerplate) === 2 && file.name === "routes")
+      )
+        return;
+
+      // Repeat the procedure again
+      copyRecursively(newFrom, newTo);
+    }
   });
+};
+
+/**
+ * Generates the Welcome message
+ *
+ * @param {string} appName string Name
+ *
+ * @return {void}
+ */
+const welcomeMsg = appName => {
+  console.log(
+    `${chalk.bold("Boilerplate is generated. \nRun ")} ${chalk.bold.cyan(
+      `cd ${appName} && npm run dev`
+    )} ${chalk.bold(`for starting dev server. \n`)}`
+  );
+  console.log(
+    chalk
+      .hex("#fdf39f")
+      .bold("\n🕸  With Great Power Comes Great Devonsibility 🚀\n")
+  );
 };
 
 /**
@@ -108,11 +141,12 @@ const copyRecursively = (from, to) => {
  */
 const createTemplate = appName => {
   const currentDirectoryPath = process.cwd() + `/${appName}`;
-  // 1 Make directory on the current path
   console.log(
-    `\n---------------------------------------------------\n
+    chalk.bold.magenta(
+      `\n---------------------------------------------------\n
                Generating Boilerplate
     \n--------------------------------------------------- \n`
+    )
   );
   copyRecursively(reactTemplatePath, currentDirectoryPath);
 };
@@ -124,22 +158,46 @@ const createTemplate = appName => {
  *
  * @return {void}
  */
-const init = appName => {
+const init = async appName => {
   console.log("Welcome to renode.🚀");
 
-  inquirer.prompt(questions).then(answers => {
-    reflections = {
-      ...answers,
-      appName
-    };
+  const answers = await inquirer.prompt(questions);
 
-    createTemplate(appName);
-  });
+  reflections = {
+    ...answers,
+    appName
+  };
+
+  createTemplate(appName);
+
+  if (!error) {
+    console.log(chalk.bold(`\nWait untill node_modules install...\n`));
+
+    exec(
+      `cd ${process.cwd()}/${appName} && npm i && cd ..`,
+      (err, stderr, stdout) => {
+        if (err) {
+          console.log(chalk.red(`\n🐞 ${err} \n`));
+        }
+
+        console.log(stdout);
+        welcomeMsg(appName);
+      }
+    );
+  }
 };
+
+program.on("--help", () => {
+  console.log(
+    chalk
+      .hex("#fdf39f")
+      .bold("\n🕸  With Great Power Comes Great Devonsibility 🕷 \n")
+  );
+});
 
 program
   .version(version, "-v, --version")
-  .command("create <type>") // sub-command name, appName = type, required
+  .command("create <project-name>") // sub-command name, appName = type, required
   .description("Make a project") // command description
   // function to execute when command is uses
   .action(init);

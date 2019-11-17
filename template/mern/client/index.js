bb143c2fc00574602fabf038e742fedf507852ef
praveen-me <praveenkrsaini0@gmail.com>
2019-11-17 16:33:19 +0530

    Add Auth HoC

--- template/mern/client/index.js
+++ template/mern/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import store from './store';
+import store from './store/store';
 import { Provider } from 'react-redux';
 import './scss/index.scss';
 

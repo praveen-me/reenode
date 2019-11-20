109fe5a210a5ccf5ac70afcb07cc98b3002c6503
praveen-me <praveenkrsaini0@gmail.com>
2019-11-21 01:24:20 +0530

    Apply AuthHoC for login and signup components

--- template/mern/client/components/hoc/AuthHoC.js
+++ template/mern/client/components/hoc/AuthHoC.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
-import Login from "../Login";
 
 const AuthHoc = Component => {
   const WrappedComponent = ({ history }) => {
     const token = useSelector(state => state.token);
 
-    if (token) {
+    if (!token) {
       return <Component />;
     } else {
-      history.push("/login");
-      return <Login />;
+      history.push("/");
+      return <></>;
     }
   };
 
